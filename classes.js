9e962b6fdbf3f75768456e4da658bae9eeafbf26
douglassilvaido <texcronos@gmail.com>
2024-05-17 21:41:32 -0300

    first commit

---
+++ classes.js
@@ -0,0 +1,65 @@
+/**O Que deve ser utilizado**
+- Variáveis
+- Operadores
+- Laços de repetição
+- Estruturas de decisões
+- Funções
+- Classes e Objetos
+
+## Objetivo:
+
+Crie uma classe generica que represente um herói de uma aventura e que possua as seguintes propriedades:
+
+- nome
+- idade
+- tipo (ex: guerreiro, mago, monge, ninja )
+
+além disso, deve ter um método chamado atacar que deve atender os seguientes requisitos:
+
+- exibir a mensagem: "o {tipo} atacou usando {ataque}")
+- aonde o {tipo} deve ser concatenando o tipo que está na propriedade da classe
+- e no {ataque} deve seguir uma descrição diferente conforme o tipo, seguindo a tabela abaixo:
+
+se mago -> no ataque exibir (usou magia)
+se guerreiro -> no ataque exibir (usou espada)
+se monge -> no ataque exibir (usou artes marciais)
+se ninja -> no ataque exibir (usou shuriken)
+
+## Saída
+
+Ao final deve se exibir uma mensagem:
+
+- "o {tipo} atacou usando {ataque}"
+  ex: mago atacou usando magia
+  guerreiro atacou usando espada 
+  */
+
+class Heroi {
+  constructor(nome, idade, tipo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+  }
+
+  atacar() {
+    var ataque;
+    if (this.tipo == "mago") {
+      ataque = "magia";
+    } else if (this.tipo == "guerreiro") {
+      ataque = "espada";
+    } else if (this.tipo == "monje") {
+      ataque = "artes marciais";
+    } else if (this.tipo == "ninja") {
+      ataque = "shuriken";
+    }
+
+    console.log(`o ${this.tipo} atacou usando ${ataque}`);
+  }
+}
+
+var panda = new Heroi("Poo", 20, "guerreiro");
+var ogoey = new Heroi("ogoey", 20, "monje");
+
+panda.atacar();
+console.log(panda)
+ogoey.atacar()
